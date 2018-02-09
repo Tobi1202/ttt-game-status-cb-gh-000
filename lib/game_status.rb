1440e91c0ec6d33e210a0525aa5f2fcda320fad3
Toby Bro <thetobiasbrodersen@gmail.com>
2018-02-09 12:41:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,14 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
 def won?(board)
@@ -26,9 +26,9 @@ end
 
 def full?(board)
   board.each{|cell|
-if cell == " "
-  return false
-end
+    if cell == " "
+      return false
+    end
   }
-return true
+  return true
 end
