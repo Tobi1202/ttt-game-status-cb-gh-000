3cf4a620697d9ad8228c07f1751d22a69fff6570
Toby Bro <thetobiasbrodersen@gmail.com>
2018-02-09 12:57:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ end
 
 def winner(board)
   wincomb = won?(board)
-  if wincomb = false || board[wincomb[0]]
+  if wincomb = false || board[wincomb[0]] == ""
     return nil
   else
     return board[wincomb[0]]
